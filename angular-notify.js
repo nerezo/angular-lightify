f9dfd8b02f53be00b92532e6ea11fc10167f3b68
Okan Özeren <okanozeren@gmail.com>
2014-11-28 15:40:41 +0200

    Names change. Variable using instead dom selector.
    Added a namespace to the names of module, directive and factories so that we they should be have more particular names. Added a container property to the factory which keeps the notification container DOM element.

--- angular-notify.js
+++ angular-notify.js
@@ -1,69 +1,89 @@
-angular.module('notification', [])
-.factory('notification', ['$rootScope', '$timeout', function($rootScope, $timeout) {
-   var messages = [];
+  angular.module('nrzNotification', [])
 
-   var reset;
+  .factory('nrzNotification', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
+    var messages = [];
 
-   var cleanup = function() {
+    var reset;
+    // Cleans all available shown messages
+    var cleanup = function () {
       $timeout.cancel(reset);
-      reset = $timeout(function() {
-         messages = [];
+      reset = $timeout(function () {
+        messages = [];
       });
-   };
+    };
 
-   var emit = function() {
-      $rootScope.$emit('flash:message', messages, notification, cleanup);
-   };
+    // Trigger the the flash:message event
+    var emit = function () {
+      $rootScope.$emit('flash:message', messages, nrzNotification, cleanup);
+    };
 
-   $rootScope.$on('$routeChangeSuccess', emit);
+    // Below events trigger on $stateChangeSuccess event with uiRouter and $routeChangeSuccess event with ngRoute
+    $rootScope.$on('$routeChangeSuccess', emit);
+    $rootScope.$on('$stateChangeSuccess', emit);
 
-   var notification = function(message, timeout) {
+    var nrzNotification = function (message, timeout) {
       switch (message.type) {
-         case 'info':
-            message.text = '<strong>Info:</strong>&nbsp;' + message.text;
-            message.fadeOut = { enabled: true, delay: (timeout === undefined ? 7000 : timeout) };
-            break;
-         case 'success':
-            message.text = '<strong>Success:</strong>&nbsp;' + message.text;
-            message.fadeOut = { enabled: true, delay: (timeout === undefined ? 7000 : timeout) };
-            break;
-         case 'warning':
-            message.text = '<strong>Warning:</strong>&nbsp;' + message.text;
-            message.fadeOut = { enabled: true, delay: (timeout === undefined ? 10000 : timeout) };
-            break;
-         case 'danger':
-            message.text = '<strong>Important:</strong>&nbsp;' + message.text;
-            message.fadeOut = { enabled: true, delay: (timeout === undefined ? 10000 : timeout) };
-            break;
+        case 'info':
+          message.text = '<strong>Info:</strong>&nbsp;' + message.text;
+          message.fadeOut = { enabled: true, delay: (timeout === undefined ? 7000 : timeout) };
+          break;
+        case 'success':
+          message.text = '<strong>Success:</strong>&nbsp;' + message.text;
+          message.fadeOut = { enabled: true, delay: (timeout === undefined ? 7000 : timeout) };
+          break;
+        case 'warning':
+          message.text = '<strong>Warning:</strong>&nbsp;' + message.text;
+          message.fadeOut = { enabled: true, delay: (timeout === undefined ? 10000 : timeout) };
+          break;
+        case 'danger':
+          message.text = '<strong>Important:</strong>&nbsp;' + message.text;
+          message.fadeOut = { enabled: true, delay: (timeout === undefined ? 10000 : timeout) };
+          break;
+        default:
+          message.text = message.text;
+          message.fadeOut = { enabled: true, delay: (timeout === undefined ? 10000 : timeout) };
+          break;
       }
 
-      $('.top-right').notify({
-         message: { html: '<span style="margin-right: 20px">' + message.text + '</span>', text: false },
-         type: message.type,
-         fadeOut: message.fadeOut
+      nrzNotification.nrzNotificationContainer.notify({
+        message: { html: '<span>' + message.text + '</span>', text: false },
+        type: message.type,
+        fadeOut: message.fadeOut
       }).show();
-   };
+    };
 
-   notification.flash = function(message, timeout) {
+    // The container variable initializes.
+    nrzNotification.nrzNotificationContainer = {};
+
+    // The flash stack updater method.
+    nrzNotification.flash = function (message, timeout) {
       messages.push({ message: message, timeout: timeout });
-   };
+    };
 
-   return notification;
-}])
-.directive('flashMessages', [function() {
-   var directive = {restrict: 'EA', replace: true};
+    return nrzNotification;
+  }])
 
-   directive.controller = ['$scope', '$rootScope', function($scope, $rootScope) {
-      $rootScope.$on('flash:message', function(_, messages, notification, done) {
-         $scope.messages = messages;
+  .directive('nrzFlashMessages', function (nrzNotification) {
+    return {
+      restrict: 'EA',
+      replace: true,
+      scope: false,
+      controller: function ($scope, $rootScope) {
+        $rootScope.$on('flash:message', function (_, messages, nrzNotification, done) {
+          // Uses all stacked messages for showing the notification bands.
+          angular.forEach(messages, function (message, index) {
+            nrzNotification(message.message, message.timeout);
+          });
 
-         angular.forEach(messages, function(message, index){
-            notification (message.message, message.timeout);
-         });
+          done();
+        });
+      },
+      link: function (scope, element, attrs) {
+        // Default and user selected css class are adding to the element.
+        element.addClass('notifications').addClass(attrs.position);
 
-         done();
-      });
-   }];
-
-   return directive;
-}]);
+        // Keeping the element to be using within the methods of the factory.
+        nrzNotification.nrzNotificationContainer = element;
+      }
+    };
+  });
